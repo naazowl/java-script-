847684338bbeeb1e0b85db06ceb2c3cfce03f2bd
nazwa <naazowl2303@gmail.com>
2025-05-27 22:17:44 +0700

    BAB5C.java

---
+++ js nazwa/js nazwa 2/BAB 5/BAB5C.java
@@ -0,0 +1,12 @@
+for (steatment 1; steatment 2; steatment 3){
+    code block to be executed
+}
+
+while (condition) {
+    code blockt to be executed
+}
+
+do {
+    code block to be executed
+}
+while(condition);
